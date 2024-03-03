c62d646382639da8da8992fe4215632a80ca6ba2
Jan Kelemen <jkeleme@gmail.com>
2024-03-03 19:37:56 +0100

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,35 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
+require_conan_version = ">=2.0"
+
+class VPConan(ConanFile):
+    name = "vp"
+    settings = "os", "compiler", "build_type", "arch"
+    version = "0.1"
+
+    exports_sources = "cmake", "src", "CMakeLists.txt", "LICENSE"
+
+    def requirements(self):
+        self.requires("fmt/10.2.1")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[^3.27]")
+        self.test_requires("catch2/[^3.5.2]")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = "ConanPresets.json"
+        tc.generate()
+
+        cmake = CMakeDeps(self)
+        cmake.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
